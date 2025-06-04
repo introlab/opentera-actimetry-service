cf2f3c8b1d189eac0d8bf13cc808c5784f5e108a
Dominic Létourneau <doumdi@gmail.com>
2025-06-04 10:56:31 -0400

    Added skeleton for API tests, will use an opentera server in test mode

---
+++ tests/API/BaseActimetryServiceAPITest.py
@@ -0,0 +1,225 @@
+import unittest
+import time
+import uuid
+import jwt
+import os
+import json
+import requests
+from datetime import date, datetime, timedelta
+
+from flask.testing import FlaskClient
+from flask import Flask
+from ConfigManager import ConfigManager
+from opentera.services.ServiceAccessManager import ServiceAccessManager
+from tests.API.FakeActimetryService import FakeActimetryService
+from libactimetry.db.models.BaseModel import BaseModel
+from libactimetry.db.DBManager import DBManager
+import Globals
+
+
+def infinite_jti_sequence():
+    num = 0
+    while True:
+        yield num
+        num += 1
+
+
+# Initialize generator, call next(user_jti_generator) to get next sequence number
+user_jti_generator = infinite_jti_sequence()
+participant_jti_generator = infinite_jti_sequence()
+
+
+class BaseSurveyJSServiceAPITest(unittest.TestCase):
+    test_endpoint = ""
+
+    @classmethod
+    def setUpClass(cls):
+        # Instance of Fake service API will create a new flask_app
+        cls._service = FakeActimetryService()
+
+        # API Need those variables to be set
+        Globals.service = cls._service
+        Globals.config_man = cls._service.config_man
+
+        # Setup SurveyJS database
+        cls._service.flask_app.debug = False
+        cls._service.flask_app.testing = True
+        cls._db_man = DBManager(app=cls._service.flask_app, test=True)
+        # Setup DB in RAM
+        # Create file in current directory
+        cls._db_man.open_local(
+            {"filename": os.path.join(os.getcwd(), "test.db")}, echo=True, ram=True
+        )
+
+        # Creating default users / tests. Time-consuming, only once per test file.
+        with cls._service.flask_app.app_context():
+            cls._db_man.create_defaults(cls._service.config_man, test=True)
+            cls.db = cls._db_man.db
+
+    def app_context(self):
+        self.assertIsNotNone(self._service)
+        self.assertIsNotNone(self._service.flask_app)
+        return self._service.flask_app.app_context()
+
+    @classmethod
+    def tearDownClass(cls):
+        with cls._service.flask_app.app_context():
+            BaseModel.metadata.drop_all(cls.db.engine)
+
+    def setUp(self):
+        self.assertIsNotNone(self._service)
+        self.assertIsNotNone(self._service.flask_app)
+        self.test_client = self._service.flask_app.test_client()
+        self.assertIsNotNone(self.test_client)
+        with self.app_context():
+            pass
+
+    def tearDown(self):
+        with self.app_context():
+            pass
+
+    @staticmethod
+    def _generate_fake_user_token(
+        name="FakeUser",
+        user_uuid=str(uuid.uuid4()),
+        role=[],
+        superadmin=False,
+        expiration=3600,
+    ):
+        # Creating token with user info
+        now = time.time()
+        token_key = ServiceAccessManager.api_user_token_key
+
+        payload = {
+            "iat": int(now),
+            "exp": int(now) + expiration,
+            "iss": "TeraServer",
+            "jti": next(user_jti_generator),
+            "user_uuid": user_uuid,
+            "role": role,
+            "id_user": 1,
+            "user_fullname": name,
+            "user_superadmin": superadmin,
+            "service_access": {"SurveyJSService": role},  # roles of the user
+        }
+
+        return jwt.encode(payload, token_key, algorithm="HS256")
+
+    @staticmethod
+    def _generate_fake_user_role(name="FakeUser", superadmin=False, expiration=3600):
+        # Creating token with user info
+        now = time.time()
+        token_key = ServiceAccessManager.api_user_token_key
+        payload = {
+            "iat": int(now),
+            "exp": int(now) + expiration,
+            "iss": "TeraServer",
+            "jti": next(user_jti_generator),
+            "id_user": 1,
+            "user_fullname": name,
+            "user_superadmin": superadmin,
+            "service_access": {"SurveyJSService": []},
+        }
+
+        return jwt.encode(payload, token_key, algorithm="HS256")
+
+    @staticmethod
+    def _generate_fake_static_participant_token(participant_uuid=str(uuid.uuid4())):
+        # Creating token with participant info
+        token_key = ServiceAccessManager.api_participant_static_token_key
+        payload = {
+            "iss": "TeraServer",
+            "jti": next(participant_jti_generator),
+            "participant_uuid": participant_uuid,
+            "id_participant": 1,
+        }
+        return jwt.encode(payload, token_key, algorithm="HS256")
+
+    @staticmethod
+    def _generate_fake_dynamic_participant_token(
+        name="FakeParticipant", participant_uuid=str(uuid.uuid4()), expiration=3600
+    ):
+        # Creating token with participant info
+        now = time.time()
+        token_key = ServiceAccessManager.api_participant_static_token_key
+        payload = {
+            "iat": int(now),
+            "exp": int(now) + expiration,
+            "iss": "TeraServer",
+            "jti": next(participant_jti_generator),
+            "participant_uuid": participant_uuid,
+            "id_participant": 2,
+            "user_fullname": name,
+        }
+
+        return jwt.encode(payload, token_key, algorithm="HS256")
+
+    def _login_user(
+        self, username: str, password: str, endpoint: str = "/api/user/login"
+    ) -> dict:
+        # 2FA is disabled for tests
+        # Websocket is disabled for tests
+        # Use HTTPAuth to login
+        server_url = f'https://{self._service.config_man.backend_config["hostname"]}:{self._service.config_man.backend_config["port"]}'
+        params = {"with_websocket": False}
+        response = requests.get(
+            server_url + endpoint,
+            auth=(username, password),
+            params=params,
+            verify=False,
+        )
+        self.assertTrue(response.status_code == 200)
+        return response.json()
+
+    def _get_participants(self, user_token: str, id_site: int):
+        params = {"full": True, "id_site": id_site}
+        response = self._service.get_from_opentera_with_token(
+            token=user_token, api_url="/api/user/participants", params=params
+        )
+        self.assertEqual(response.status_code, 200)
+        return response.json()
+
+    def _get_with_token_auth(
+        self,
+        client: FlaskClient,
+        token: str = "",
+        params=None,
+        language="fr",
+        endpoint=None,
+    ):
+        if params is None:
+            params = {}
+        if endpoint is None:
+            endpoint = self.test_endpoint
+        headers = {"Authorization": "OpenTera " + token}
+        headers["Accept-Language"] = language
+        return client.get(endpoint, headers=headers, query_string=params)
+
+    def _post_with_token_auth(
+        self,
+        client: FlaskClient,
+        token: str = "",
+        json: dict = None,
+        params: dict = None,
+        endpoint: str = None,
+    ):
+        if params is None:
+            params = {}
+        if endpoint is None:
+            endpoint = self.test_endpoint
+        headers = {"Authorization": "OpenTera " + token}
+        return client.post(endpoint, headers=headers, query_string=params, json=json)
+
+    def _delete_with_token_auth(
+        self,
+        client: FlaskClient,
+        token: str = "",
+        params: dict = None,
+        endpoint: str = None,
+    ):
+        if params is None:
+            params = {}
+        if endpoint is None:
+            endpoint = self.test_endpoint
+        headers = {"Authorization": "OpenTera " + token}
+        return client.delete(endpoint, headers=headers, query_string=params)
---
+++ tests/API/FakeActimetryService.py
@@ -0,0 +1,332 @@
+import uuid
+from io import BytesIO
+import time
+import jwt
+import json
+import requests
+from requests import Response
+from datetime import datetime
+
+from flask import Response as FlaskResponse
+from flask import Flask
+from flask import jsonify
+from flask_babel import Babel
+import redis
+
+import opentera.messages.python as messages
+from opentera.modules.BaseModule import BaseModule
+from opentera.services.ServiceOpenTeraWithAssets import ServiceOpenTeraWithAssets
+from opentera.redis.RedisVars import RedisVars
+from opentera.services.ServiceAccessManager import ServiceAccessManager
+
+from FlaskModule import FlaskModule, get_locale, get_timezone
+from FlaskModule import CustomAPI, authorizations
+from ConfigManager import ConfigManager
+
+
+class FakeFlaskModule(BaseModule):
+    def __init__(self, config: ConfigManager, flask_app, service):
+        BaseModule.__init__(self, "FakeFlaskModule", config)
+
+        self.flask_app = flask_app
+        self.service = service
+        self.api = CustomAPI(
+            self.flask_app,
+            version="1.0.0",
+            title="FakeActimetryService API",
+            description="FakeActimetryService API Documentation",
+            doc="/doc",
+            prefix="/api",
+            authorizations=authorizations,
+        )
+        self.api_ns = self.api.namespace("", description="FakeActimetryService API")
+
+        self.babel = Babel(
+            self.flask_app,
+            locale_selector=get_locale,
+            timezone_selector=get_timezone,
+            default_domain="opentera-surveyjs-service",
+        )
+
+        self.flask_app.debug = False
+        self.flask_app.testing = True
+        self.flask_app.secret_key = str(uuid.uuid4())  # Normally service UUID
+        self.flask_app.config.update({"SESSION_TYPE": "redis"})
+        redis_url = redis.from_url(
+            "redis://%(username)s:%(password)s@%(hostname)s:%(port)s/%(db)s"
+            % self.config.redis_config
+        )
+
+        self.flask_app.config.update({"SESSION_REDIS": redis_url})
+        self.flask_app.config.update({"BABEL_DEFAULT_LOCALE": "fr"})
+        self.flask_app.config.update({"SESSION_COOKIE_SECURE": True})
+        self.flask_app.config.update({"UPLOAD_FOLDER": "."})
+
+        # Disable flask cache
+        self.flask_app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+        self.setup_fake_actimetry_service_api(flask_app)
+
+    def setup_fake_actimetry_service_api(self, flask_app):
+        with flask_app.app_context():
+            # Setup Fake Service API
+            additional_args = {"test": True, "service": self.service}
+            FlaskModule.init_api(self.service, self, self.api_ns, additional_args)
+
+
+class FakeActimetryService(ServiceOpenTeraWithAssets):
+    """
+    The only thing we want here is a way to simulate communication with the base server.
+    We will simulate the service API with the database.
+    """
+
+    service_token = str()
+
+    def __init__(self):
+        self.flask_app = Flask("FakeActimetryService")
+        self.config_man = ConfigManager()
+        self.config_man.create_defaults()
+
+        self.redis = redis.Redis(
+            host=self.config_man.redis_config["hostname"],
+            port=self.config_man.redis_config["port"],
+            db=self.config_man.redis_config["db"],
+            username=self.config_man.redis_config["username"],
+            password=self.config_man.redis_config["password"],
+            client_name=self.__class__.__name__,
+        )
+
+        # Will reinitialize the database and create the service
+        self.reset_tera_server_database_and_create_service()
+        self.setup_site_access_to_service(site_id=1)
+
+        with self.flask_app.app_context():
+            # In case we are running without opentera server running
+            self.setup_service_access_manager()
+
+            # Get service info from redis
+            service_info = self.redis.get(
+                RedisVars.RedisVar_ServicePrefixKey
+                + self.config_man.service_config["service_key"]
+            )
+
+            # Create a fake uuid if not set in redis
+            id_service = 1
+            if service_info:
+                service_info = json.loads(service_info)
+                if "service_uuid" in service_info:
+                    self.config_man.service_config["ServiceUUID"] = service_info[
+                        "service_uuid"
+                    ]
+                    self.service_uuid = service_info["service_uuid"]
+                if "id_service" in service_info:
+                    id_service = service_info["id_service"]
+
+            # Redis variables & db must be initialized before
+            ServiceOpenTeraWithAssets.__init__(
+                self,
+                self.config_man,
+                {
+                    "service_key": self.config_man.service_config["service_key"],
+                    "id_service": id_service,
+                },
+            )
+
+        # Setup modules
+        self.flask_module = FakeFlaskModule(self.config_man, self.flask_app, self)
+
+    def reset_tera_server_database_and_create_service(self):
+        # Reset database
+        server_url = f'https://{self.config_man.backend_config["hostname"]}:{self.config_man.backend_config["port"]}'
+        response = requests.get(f"{server_url}/api/test/database/reset", verify=False)
+        if response.status_code != 200:
+            # TODO Raise exception
+            pass
+
+        # Create service
+        from tools.create_actimetry_service import create_service
+
+        if not create_service(
+            "admin", "admin", server_url, self.config_man.service_config["service_key"]
+        ):
+            # TODO Raise exception
+            pass
+
+    def setup_site_access_to_service(
+        self, site_id: int, update_projects: bool = True
+    ) -> bool:
+        server_url = f'https://{self.config_man.backend_config["hostname"]}:{self.config_man.backend_config["port"]}'
+        # Setup service for all sites and projects
+        params = {"with_websocket": False}
+        response = requests.get(
+            f"{server_url}/api/user/login",
+            auth=("admin", "admin"),
+            params=params,
+            verify=False,
+        )
+        if response.status_code == 200 and "user_token" in response.json():
+            token = response.json()["user_token"]
+
+            # Get Service id SurveyJSService
+            params = {"service_key": "SurveyJSService"}
+            response = self.get_from_opentera_with_token(
+                token, "/api/user/services", params=params
+            )
+            if response.status_code == 200 and len(response.json()) > 0:
+                service_info = response.json()[0]
+
+                # Get site with site_id
+                params = {"id_site": site_id}
+                response = self.get_from_opentera_with_token(
+                    token, "/api/user/sites", params=params
+                )
+                if response.status_code == 200 and len(response.json()) > 0:
+                    site = response.json()[0]
+
+                    # Add service to site
+                    json_data = {
+                        "service": {
+                            "id_service": service_info["id_service"],
+                            "sites": [site],
+                        }
+                    }
+                    response = self.post_to_opentera_with_token(
+                        token, "/api/user/services/sites", json_data=json_data
+                    )
+                    if response.status_code != 200:
+                        # TODO Raise exception
+                        pass
+
+                    # Get all projects for this site
+                    params = {"id_site": site_id}
+                    response = self.get_from_opentera_with_token(
+                        token, "/api/user/projects", params=params
+                    )
+                    if response.status_code == 200 and len(response.json()) > 0:
+                        projects = response.json()
+
+                        # Add service to all project (and sites...)
+                        json_data = {
+                            "service": {
+                                "id_service": service_info["id_service"],
+                                "projects": projects,
+                            }
+                        }
+                        response = self.post_to_opentera_with_token(
+                            token, "/api/user/services/projects", json_data=json_data
+                        )
+                        if response.status_code != 200:
+                            # TODO Raise exception
+                            pass
+
+                        return True
+
+        # Something went wrong
+        return False
+
+    def setup_service_access_manager(self):
+        # Initialize service from redis, posing as FileTransferService
+        # User token key (dynamic)
+        if self.redis.get(RedisVars.RedisVar_UserTokenAPIKey) is None:
+            ServiceAccessManager.api_user_token_key = str(uuid.uuid4())
+            self.redis.set(
+                RedisVars.RedisVar_UserTokenAPIKey,
+                ServiceAccessManager.api_user_token_key,
+            )
+
+        # Participant token key from DB (static)
+        if self.redis.get(RedisVars.RedisVar_ParticipantStaticTokenAPIKey) is None:
+            ServiceAccessManager.api_participant_static_token_key = str(uuid.uuid4())
+            self.redis.set(
+                RedisVars.RedisVar_ParticipantStaticTokenAPIKey,
+                ServiceAccessManager.api_participant_static_token_key,
+            )
+
+        # Participant token key (dynamic)
+        if self.redis.get(RedisVars.RedisVar_ParticipantTokenAPIKey) is None:
+            ServiceAccessManager.api_participant_token_key = str(uuid.uuid4())
+            self.redis.set(
+                RedisVars.RedisVar_ParticipantTokenAPIKey,
+                ServiceAccessManager.api_participant_token_key,
+            )
+
+        # Device Token Key from DB (static)
+        if self.redis.get(RedisVars.RedisVar_DeviceStaticTokenAPIKey) is None:
+            ServiceAccessManager.api_device_static_token_key = str(uuid.uuid4())
+            self.redis.set(
+                RedisVars.RedisVar_DeviceStaticTokenAPIKey,
+                ServiceAccessManager.api_device_static_token_key,
+            )
+
+        # Device Token Key (dynamic = static)
+        if self.redis.get(RedisVars.RedisVar_DeviceTokenAPIKey) is None:
+            ServiceAccessManager.api_device_token_key = str(uuid.uuid4())
+            self.redis.set(
+                RedisVars.RedisVar_DeviceTokenAPIKey,
+                ServiceAccessManager.api_device_token_key,
+            )
+
+        # Service Token Key (dynamic)
+        if self.redis.get(RedisVars.RedisVar_ServiceTokenAPIKey) is None:
+            ServiceAccessManager.api_service_token_key = str(uuid.uuid4())
+            self.redis.set(
+                RedisVars.RedisVar_ServiceTokenAPIKey,
+                ServiceAccessManager.api_service_token_key,
+            )
+
+    @staticmethod
+    def convert_to_standard_request_response(flask_response: FlaskResponse):
+        result = Response()
+        result.status_code = flask_response.status_code
+        result.headers = flask_response.headers
+        result.encoding = flask_response.content_encoding
+        result.raw = BytesIO(flask_response.data)
+        return result
+
+    def get_from_opentera_with_token(
+        self, token: str, api_url: str, params: dict = {}, additional_headers: dict = {}
+    ) -> Response:
+        server_url = f'https://{self.config_man.backend_config["hostname"]}:{self.config_man.backend_config["port"]}'
+        headers = {"Authorization": f"OpenTera {token}"}
+        headers.update(additional_headers)
+        return requests.get(
+            f"{server_url}{api_url}", headers=headers, params=params, verify=False
+        )
+
+    def post_to_opentera_with_token(
+        self,
+        token: str,
+        api_url: str,
+        json_data: dict,
+        params: dict = {},
+        additional_headers: dict = {},
+    ) -> Response:
+        headers = {"Authorization": f"OpenTera {token}"}
+        headers.update(additional_headers)
+        server_url = f'https://{self.config_man.backend_config["hostname"]}:{self.config_man.backend_config["port"]}'
+        return requests.post(
+            f"{server_url}{api_url}",
+            headers=headers,
+            json=json_data,
+            params=params,
+            verify=False,
+        )
+
+    def delete_from_opentera_with_token(
+        self, token: str, api_url: str, params: dict, additional_headers: dict = {}
+    ) -> Response:
+        headers = {"Authorization": f"OpenTera {token}"}
+        headers.update(additional_headers)
+        server_url = f'https://{self.config_man.backend_config["hostname"]}:{self.config_man.backend_config["port"]}'
+        return requests.delete(
+            f"{server_url}{api_url}", headers=headers, params=params, verify=False
+        )
+
+    def asset_event_received(self, event: messages.DatabaseEvent):
+        pass
+
+
+if __name__ == "__main__":
+    service = FakeActimetryService()
+    with service.flask_app.app_context():
+        pass
