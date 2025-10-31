205b74134070eb2c6c5409ca8dc89a67951ee31b
Simon Briere <simon.briere@usherbrooke.ca>
2025-10-31 11:22:07 -0400

    Added event log when scheduling processing worker.

--- libactimetry/workers/WorkerManager.py
+++ libactimetry/workers/WorkerManager.py
@@ -93,6 +93,8 @@ class WorkerManager:
                         process_worker.worker_start_time = datetime.datetime.now() + datetime.timedelta(days=1)
                         process_worker.id_database = id_database
                         ActimetryWorkerLog.insert(process_worker)
+                        Globals.service.logger.log_info('ActimetryService.WorkerManager','Scheduled processing worker (Fraysse 2021)',
+                                                        str(process_worker.worker_start_time))
 
                 del self._processes[worker_uuid]
 
